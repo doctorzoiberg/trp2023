9152e248a8bc88f1f71c819fbd509edf9fe21c9c
doctorzoiberg <129968489+doctorzoiberg@users.noreply.github.com>
2023-09-26 19:48:25 +0300

    Update prog.py

--- work02/prog.py
+++ work02/prog.py
@@ -1,1 +1,24 @@
-mat sim
+import random
+import time
+import os
+
+def generate_random_character():
+    return random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?')
+
+def generate_matrix_line(width):
+    return ''.join(generate_random_character() for _ in range(width))
+
+def matrix_animation(rows, cols, speed):
+    os.system('clear')  # Очистить экран перед началом анимации
+
+    for _ in range(rows):
+        print(generate_matrix_line(cols))
+        time.sleep(speed)
+        os.system('clear')  # Очистить экран перед выводом следующей строки
+
+# Задаем параметры анимации
+rows = 20  # Количество строк
+cols = 80  # Количество символов в каждой строке
+speed = 0.05  # Скорость анимации (в секундах)
+
+matrix_animation(rows, cols, speed)
